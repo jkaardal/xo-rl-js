3c1a42eddbafb313c99118589b0182f31b1c419d
Joel Kaardal <jkaardal@gmail.com>
2020-04-02 22:09:51 -0700

    started using indexing to get rewards from prior states

--- rewards.js
+++ rewards.js
@@ -5,9 +5,12 @@
  * 
  * @param {TicTacToe} game An instantiation of TicTacToe. 
  * @param {string} player Player token from 'X' and 'O'.
+ * @param {number} [horizon=null] When horizon is null, compute the reward
+ *     on the current state. If horizon is an integer, compute the reward
+ *     on the game's stateHistory indexed by horizon.
  */
-export function terminalOnly(game, player) {
-    let outcome = game.checkTermination();
+export function terminalOnly(game, player, horizon=null) {
+    let outcome = game.checkTermination(horizon);
     if (outcome == player) {
         return 1.0;
     } else if (outcome == 'draw' || !outcome) {
